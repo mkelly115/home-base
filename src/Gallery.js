dc543241a6aa2ec52b3e416adc884b697e42d4ad
Michael <mikek222@gmail.com>
2025-04-04 14:26:56 -0400

    updated to have interval based photo scrolling

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
-import PICTURES from './data/pictures'
+import PICTURES from "./data/pictures";
 
-export default function Gallery(){
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
 
-    const [index, setIndex] = useState(0);
-    
+  useEffect(() => {
+    setInterval(() => {
+      setIndex(storedIndex => {
+        return (storedIndex + 1) % PICTURES.length;
+      });
+    }, 3000);
+  }, []);
 
-
-    return(
-        <div className="gallery">
-            <img 
-            src={PICTURES[index].image}
-            alt="gallery"
-            />
-        </div>
-    )
-
-
+  return (
+    <div className="gallery">
+      <img src={PICTURES[index].image} alt="gallery" />
+    </div>
+  );
 }
